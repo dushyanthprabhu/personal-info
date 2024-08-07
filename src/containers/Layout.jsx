4ec9cd925ee6ae56653bc6c67bb2df9715a41e2a
dushyanth <dushyanth.prabhu@niveussolutions.com>
2024-08-07 21:06:28 +0530

    unwanted code removed

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,12 @@
+import './layout.scss';
+
+function Layout() {
+  return (
+    <div className="main-container">
+      <div className='left-container'>Left</div>
+      <div className='right-container'>Right</div>
+    </div>
+  );
+}
+
+export default Layout;
